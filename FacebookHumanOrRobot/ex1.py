54b563ebd72bfdaff3f0f2a8bb20cdf4f5492ac2
ashryOlarm <ahmed.ashry@olarm.com>
2019-08-15 18:52:53 +0200

    Organizing

---
+++ FacebookHumanOrRobot/ex1.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import scipy.optimize as opt
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import make_scorer, accuracy_score
+
+
+
